f9ce35d9b8a9eab6670f53a808780f9344d66c46
Thomas Mylott <mylottiv@gmail.com>
2019-09-15 13:29:36 -0500

    Added basic map functionality including browser based geolocation

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,8 +1,8 @@
-function initMap(){
+$.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyBWQ-sFtacE3m0IMYrFlP7w_dgNQpL-bBw', function(){
     // Initializes map upon loading of Google Maps API (see script callback in index.html)
-    const map = new google.maps.Map($('#map'), {
+    const map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 0, lng: 0},
-        zoom: 12
+        zoom: 13
     }) 
     let userLat = 0;
     let userLng = 0;
@@ -10,12 +10,12 @@ function initMap(){
     // Calls W3C geolocation feature of user's browser
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-        userLat = position.coords.latitude;
-        userLng = position.coords.longitude;
-        // Casts user coordinates as a Google Maps LatLng object
-        latLng = new google.maps.LatLng(userLat, userLng);
-        // Centers the Map on user
-        map.setCenter(latLng);
+            userLat = position.coords.latitude;
+            userLng = position.coords.longitude;
+            // Casts user coordinates as a Google Maps LatLng object
+            latLng = new google.maps.LatLng(userLat, userLng);
+            // Centers the Map on user
+            map.setCenter(latLng);
         })
-    } 
-}
+    }
+})
